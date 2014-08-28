d6b22f537f081dfc5b1816e78f0e27d8e857dc4f
fkostadinov <fkostadinov@gmx.ch>
2014-08-28 17:52:27 +0200

    Updated converter to add leading zeros

--- XmlToJsonConverter/src/XmlToMultipleJsonConverter3.java
+++ XmlToJsonConverter/src/XmlToMultipleJsonConverter3.java
@@ -17,6 +17,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import org.xml.sax.SAXException;
 
 import net.sf.json.JSONArray;
+import net.sf.json.JSONNull;
 import net.sf.json.JSONObject;
 import net.sf.json.xml.XMLSerializer;
 
@@ -31,12 +32,16 @@ public class XmlToMultipleJsonConverter3 {
 
 	private static final String JSON_ATTR_ROW = "ROW";
 
-	private static final String JSON_ATTR_ID = "ID";
+	private static final String JSON_ATTR_ID = "id";
 	private static final String JSON_ATTR_RECORDID = "@RECORDID";
 	private static final String JSON_ATTR_MODID = "@MODID";
-	private static final String JSON_ATTR_NAMEDEUTSCH = "Name_Deutsch";
-
-	private static final String OUT_JSON_FILE_POSTAMBLE = "-edb.json";
+	private static final String JSON_ATTR_NAMEDEUTSCH = "name";
+	private static final String JSON_ATTR_COMBINEDPRODUCT = "combined-product";
+	private static final String JSON_ATTR_BACKGROUND_INFO = "background-info";
+	private static final String JSON_ATTR_CALC_PROC_DOCU = "calculation-process-documentation";
+	private static final String JSON_ATTR_CO2VALUE = "co2-value";
+	
+	private static final String OUT_JSON_FILE_POSTAMBLE = "-prod.json";
 	
 	
 	/**
@@ -63,7 +68,11 @@ public class XmlToMultipleJsonConverter3 {
 				
 				JSONObject row = rows.getJSONObject(i);
 				String fileName = getOutJsonFileName(outDir, row, OUT_JSON_FILE_POSTAMBLE);
-				replaceInvalidProseKeys(row);
+				//replaceInvalidProseKeys(row);
+				replaceCombinedProdValueWithBoolean(row);
+				removeFilemakerIds(row);
+				mergeBackgroundInfoIntoCalcProcessDocu(row);
+				addMissingLeadingZeroes(row);
 				String content = prettyPrintJson ? row.toString(2) : row.toString();
 				writeJsonFile(fileName, content, outJsonFileEncoding, true);
 
@@ -74,6 +83,93 @@ public class XmlToMultipleJsonConverter3 {
 		} catch (ParserConfigurationException e) {
 			e.printStackTrace(System.err);
 		}
+	}
+	
+	/** Sets a boolean value for combined products (either true or false) according to whether
+	 *  a value is set or not in the JSON Object. */
+	private void replaceCombinedProdValueWithBoolean(JSONObject json) {
+		// combined-product -> boolean value
+		Object v = json.remove(JSON_ATTR_COMBINEDPRODUCT);
+
+		if (v instanceof JSONNull) {
+			// It's not a combined product, thus set false
+			json.put(JSON_ATTR_COMBINEDPRODUCT, false);
+		}
+		else if (v instanceof JSONObject || v instanceof String) {
+			// It's a combined product, thus set true
+			json.put(JSON_ATTR_COMBINEDPRODUCT, true);
+		}
+		else {
+			throw new IllegalArgumentException("Json object value has illegal class. It is neither JSONObject nor JSONNull but " + v.getClass().getName());
+		}
+	}
+	
+	/** Completely removes Filemaker IDs @RECORDID and @MODID */
+	private void removeFilemakerIds(JSONObject json) {
+		json.remove(JSON_ATTR_RECORDID);
+		json.remove(JSON_ATTR_MODID);
+	}
+	
+	/** Merges background-info into calculation-process-documentation */
+	private void mergeBackgroundInfoIntoCalcProcessDocu(JSONObject json) {
+		Object v1 = json.remove(JSON_ATTR_BACKGROUND_INFO);
+		Object v2 = json.get(JSON_ATTR_CALC_PROC_DOCU);
+		
+		// Case 1: v1 is a JSONNull or null
+		if (v1 instanceof JSONNull || v1 == null) {
+			// Nothing to do
+		}
+		
+		// Case 2: v1 is an instance of JSONObject
+		else if (v1 instanceof JSONObject) {
+			JSONObject o1 = (JSONObject) v1;
+			if (o1.isEmpty()) {
+				// Nothing to do
+			}
+			else {
+				throw new IllegalArgumentException("Json object value has illegal class. It's not a String but " + v1.getClass().getName());						
+			}
+		}
+		
+		// Case 3: v1 is an instance of String
+		else if (v1 instanceof String) {
+			
+			// Case 3.1: v1 is an instance of String and v2 is an instance of JSONNull
+			if (v2 instanceof JSONNull || v2 == null) {
+				// Simply replace v2 with v1
+				json.put(JSON_ATTR_CALC_PROC_DOCU, v1);
+			}
+			
+			// Case 3.2: v1 is an instance of String and v2 is an instance of JSONObject
+			else if (v2 instanceof JSONObject) {
+				JSONObject o2 = (JSONObject) v2;
+				if (o2.isEmpty()) {
+					// Nothing to do
+				}
+				else {
+					throw new IllegalArgumentException("Json object value has illegal class. It's not a String but " + v2.getClass().getName());						
+				}			
+			}
+			
+			// Case 3.3: v1 is an instance of String and v2 is an instance of String
+			else if (v2 instanceof String) {
+				// Append v1 to v2
+				v2 = new StringBuilder().append(v2).append(NEWLINE).append(NEWLINE).append(v1).toString();
+				json.put(JSON_ATTR_CALC_PROC_DOCU, v2);
+			}
+			
+			// Case 3.4: v1 is an instance of String and v2 is something else
+			else {
+				throw new IllegalArgumentException("Json object value has illegal class. It's not a String but " + v2.getClass().getName());							
+			}
+			
+		}
+		
+		// Case 4: v1 is something else
+		else {
+			throw new IllegalArgumentException("Json object value has illegal class. It's not a String but " + v1.getClass().getName());			
+		}
+		
 	}
 	
 	/**
@@ -90,6 +186,33 @@ public class XmlToMultipleJsonConverter3 {
 		// @MODID -> MODID
 		v = json.remove(JSON_ATTR_MODID);
 		json.put(JSON_ATTR_MODID.substring(1, JSON_ATTR_MODID.length()), v);
+	}
+	
+	/**
+	 * This methods scans for CO2 values missing a leading zero, e.g. ".03"
+	 * @param json
+	 */
+	private void addMissingLeadingZeroes(JSONObject json) {
+		Object v = json.get(JSON_ATTR_CO2VALUE);
+		if (v instanceof JSONNull || v == null) {
+			//throw new IllegalArgumentException("No CO2 value set for id " + json.get(JSON_ATTR_ID));		
+			json.put(JSON_ATTR_CO2VALUE, "-999999999");
+			System.out.println("WARNING: No CO2 value set for id " + json.get(JSON_ATTR_ID));
+		}
+		else if (v instanceof String) {
+			String s = (String) v;
+			if (s.trim().matches("^[.][0-9]+")) {
+				String s2 = "0" + s.trim();
+				System.out.println("Replacing " + s + " with " + s2);
+				json.put(JSON_ATTR_CO2VALUE, s2);
+			}
+		}
+		else if (v instanceof JSONObject) {
+			throw new IllegalArgumentException("Json object value has illegal class. It's not a String but " + v.getClass().getName());			
+		}
+		else {
+			throw new IllegalArgumentException("Json object value has illegal class. It's not a String but " + v.getClass().getName());			
+		}
 	}
 
 	/**
@@ -233,7 +356,7 @@ public class XmlToMultipleJsonConverter3 {
 	 */
 	public static void main(String[] args) throws IOException {
 		new XmlToMultipleJsonConverter3().split(
-				"./xml/filemaker-export-kostadinov-komplett-utf8.xml",
+				"./xml/2014-08-28-final-filemaker-export.xml",
 				"UTF-8",
 				"./json/",
 				"UTF-8",
